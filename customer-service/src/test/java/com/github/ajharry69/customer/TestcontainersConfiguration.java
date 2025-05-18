2e8247cd5a867f003edd786d825c164e31f60f66
Orinda Harrison (Mitch) <mitchke@duck.com>
2025-05-19 01:35:24 +0300

    Generate test data

--- customer-service/src/test/java/com/github/ajharry69/customer/TestcontainersConfiguration.java
+++ customer-service/src/test/java/com/github/ajharry69/customer/TestcontainersConfiguration.java
@@ -1,36 +1,70 @@
 package com.github.ajharry69.customer;
 
+import com.github.ajharry69.customer.models.Customer;
+import lombok.extern.slf4j.Slf4j;
+import net.datafaker.Faker;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.utility.DockerImageName;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @TestConfiguration(proxyBeanMethods = false)
+@Slf4j
 class TestcontainersConfiguration {
+    private final Faker faker = new Faker();
 
-	@Bean
-	@RestartScope
-	@ServiceConnection
-	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:17-alpine"));
-	}
+    @Bean
+    CommandLineRunner generateSampleData(final CustomerRepository repository) {
+        return _ -> generateDataAsync(repository);
+    }
 
-	@Bean
-	@RestartScope
-	@ServiceConnection
-	RabbitMQContainer rabbitContainer() {
-		return new RabbitMQContainer(DockerImageName.parse("rabbitmq:4.1-alpine"));
-	}
+    @Async
+    public void generateDataAsync(CustomerRepository repository) {
+        List<Customer> customers = new ArrayList<>();
+        int numCustomers = new Random().nextInt(10, 100);
+        log.info("Generating {} customers...", numCustomers);
+        for (int i = 0; i < numCustomers; i++) {
+            customers.add(
+                    Customer.builder()
+                            .firstName(faker.name().firstName())
+                            .lastName(faker.name().lastName())
+                            .build()
+            );
+        }
+        log.info("Saving {} customers...", numCustomers);
+        repository.saveAll(customers);
+        log.info("Saved {} customers", numCustomers);
+    }
 
-	@Bean
-	@RestartScope
-	@ServiceConnection(name = "redis")
-	GenericContainer<?> redisContainer() {
-		return new GenericContainer<>(DockerImageName.parse("redis:8.0-alpine")).withExposedPorts(6379);
-	}
+    @Bean
+    @RestartScope
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:17-alpine"));
+    }
+
+    @Bean
+    @RestartScope
+    @ServiceConnection
+    RabbitMQContainer rabbitContainer() {
+        return new RabbitMQContainer(DockerImageName.parse("rabbitmq:4.1-alpine"));
+    }
+
+    @Bean
+    @RestartScope
+    @ServiceConnection(name = "redis")
+    GenericContainer<?> redisContainer() {
+        return new GenericContainer<>(DockerImageName.parse("redis:8.0-alpine")).withExposedPorts(6379);
+    }
 
 }
