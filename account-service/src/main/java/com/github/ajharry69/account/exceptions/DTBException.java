6f4e0e9926134dbdb909a89e124c2c7a877f0765
Orinda Harrison (Mitch) <mitchke@duck.com>
2025-05-19 02:19:17 +0300

    Use standard `ResponseStatusException` from spring boot as base exception class

--- account-service/src/main/java/com/github/ajharry69/account/exceptions/DTBException.java
+++ account-service/src/main/java/com/github/ajharry69/account/exceptions/DTBException.java
@@ -2,13 +2,16 @@ package com.github.ajharry69.account.exceptions;
 
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.server.ResponseStatusException;
 
 @Getter
-public class DTBException extends RuntimeException {
+public class DTBException extends ResponseStatusException {
     private final HttpStatus httpStatus;
     private final String errorCode;
 
     public DTBException(HttpStatus httpStatus, String errorCode) {
+        super(HttpStatusCode.valueOf(httpStatus.value()));
         this.httpStatus = httpStatus;
         this.errorCode = errorCode;
     }
