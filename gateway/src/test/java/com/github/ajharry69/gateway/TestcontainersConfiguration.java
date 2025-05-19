327b34d35879d11dcf0a8e3250e603398e9b62c8
Orinda Harrison (Mitch) <mitchke@duck.com>
2025-05-19 12:07:04 +0300

    Drop unnecessary testcontainers support

--- gateway/src/test/java/com/github/ajharry69/gateway/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.github.ajharry69.gateway;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
