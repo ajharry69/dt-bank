9adaa5d55c479416ad613bfb4482f002cd3e1e2e
Orinda Harrison (Mitch) <mitchke@duck.com>
2025-05-20 08:26:32 +0300

    Activate test profile

--- account-service/src/test/java/com/github/ajharry69/account/AccountControllerTest.java
+++ account-service/src/test/java/com/github/ajharry69/account/AccountControllerTest.java
@@ -34,7 +34,7 @@ import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, properties = "spring.profiles.active=test")
 class AccountControllerTest {
     private static final Faker faker = new Faker();
     private static final String iban = iban();
