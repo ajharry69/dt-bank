6ad476c72d0194739dd287e2de962c18abfebc21
Orinda Harrison (Mitch) <mitchke@duck.com>
2025-07-13 15:10:29 +0300

    Rename GitHub workload identity pool

--- ops/terraform/main.tf
+++ ops/terraform/main.tf
@@ -52,13 +52,13 @@ resource "google_project_iam_member" "gke_developer" {
 }
 
 # --- Workload Identity Federation (Securely connect GitHub Actions to GCP) ---
-resource "google_iam_workload_identity_pool" "pool" {
-  workload_identity_pool_id = "github-pool"
+resource "google_iam_workload_identity_pool" "github" {
+  workload_identity_pool_id = "github-actions-pool"
   display_name              = "GitHub Actions Pool"
 }
 
 resource "google_iam_workload_identity_pool_provider" "github_provider" {
-  workload_identity_pool_id          = google_iam_workload_identity_pool.pool.workload_identity_pool_id
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github.workload_identity_pool_id
   workload_identity_pool_provider_id = "github-provider"
   display_name                       = "GitHub Actions Provider"
   attribute_condition                = "assertion.repository=='${var.github_repo}'"
@@ -75,5 +75,5 @@ resource "google_iam_workload_identity_pool_provider" "github_provider" {
 resource "google_service_account_iam_member" "workload_identity_user" {
   service_account_id = google_service_account.github_actions_sa.name
   role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.pool.name}/attribute.repository/${var.github_repo}"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.repository/${var.github_repo}"
 }
